23b09efdc39e17021f43b60704ad5b815bdbf6b7
Marina <marinaacerbi@hotmail.com>
2025-12-13 21:02:14 -0300

    Update main.py
    Add set score logic

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ print("=======================================================")
 print("Bienvenido al programa de gestión de partidos de Voley.")
 print("=======================================================")
 
+# pedir los puntos de los equipos al usuario
+def pedir_puntos(equipo):
+    puntos = int(input(f"Ingrese los puntos del equipo {equipo}: "))
+    return puntos
+
 # pedir nombres de los equipos
 def main():
     equipo_a = pedir_equipo("A")
@@ -16,6 +21,30 @@ def main():
 # mostrar los equipos registrados
     print("-----------------------------------------------------------------")
     print(f"Los equipos registrados son: Local: {equipo_a} y Visitante: {equipo_b}.")
+    print("-----------------------------------------------------------------")
+
+# cargar los puntos de cada set
+    num_set = 1
+    set_a = 0
+    set_b = 0
+
+    while num_set <= 5 and set_a < 3 and set_b < 3:
+        print(f"Set {num_set}:")
+        puntos_a = pedir_puntos(equipo_a)
+        puntos_b = pedir_puntos(equipo_b)
+        if puntos_a > puntos_b:
+            print(f"Gana el set {equipo_a}")
+            set_a += 1
+            set += 1
+        elif puntos_b > puntos_a:
+            print(f"Gana el set {equipo_b}")
+            set_b += 1
+            set += 1
+        else:
+            print("Error: Los puntos no pueden ser iguales en un set de Voley.")
+
+    print("-----------------------------------------------------------------")
+    print(f"Resultado final: {equipo_a} {set_a} - {set_b} {equipo_b}")
     print("-----------------------------------------------------------------")
 
 if __name__ == "__main__":
